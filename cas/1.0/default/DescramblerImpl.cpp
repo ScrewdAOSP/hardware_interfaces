958a8f69831e30e5fd2c83023193b9706ab1099c
wrongway213 <tropeazy@gmail.com>
2018-07-06 03:32:52 +0530

    Fix 'holder' build errors from r41 merge

--- cas/1.0/default/DescramblerImpl.cpp
+++ cas/1.0/default/DescramblerImpl.cpp
@@ -188,7 +188,6 @@ Return<void> DescramblerImpl::descramble(
             dstOffset,
             NULL);
 
-    holder.reset();
     _hidl_cb(toStatus(result >= 0 ? OK : result), result, NULL);
     return Void();
 }
